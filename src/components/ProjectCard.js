da538af92a5f363f417e632a103e3fa13727b7f3
Lovisa Svensson <77107017+spookypannkaka@users.noreply.github.com>
2024-02-19 21:00:49 +0100

    Adjusted look of project cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,31 +1,115 @@
 import '../styles/Card.css'
 
+function DurationIcon() {
+    return (
+        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm0 11h6v1h-7v-9h1v8z" fill="#D4FAFF"/></svg>
+    );
+}
+
+function PeopleIcon() {
+    return (
+<svg
+   viewBox="0 0 100 100"
+   x="0px"
+   y="0px"
+   version="1.1"
+   width="30"
+   height="30">
+  <g
+     id="g148"
+     transform="matrix(1.6480449,0,0,1.6480449,-2.7779358,-3.690144)">
+    <path
+       d="M 23.71,21.59 A 8.29,8.29 0 1 0 32,13.3 8.3,8.3 0 0 0 23.71,21.59 Z m 14.58,0 A 6.29,6.29 0 1 1 32,15.3 a 6.29,6.29 0 0 1 6.29,6.29 z"
+       id="path64" fill="#D4FAFF"/>
+    <path
+       d="m 48.92,30.73 a 12,12 0 0 0 -7.85,2.92 15.21,15.21 0 0 0 -18.14,0 A 12.06,12.06 0 0 0 3,42.81 v 2.92 a 1,1 0 0 0 1,1 h 12.71 v 3 a 1,1 0 0 0 1,1 h 28.58 a 1,1 0 0 0 1,-1 v -3 H 60 a 1,1 0 0 0 1,-1 V 42.81 A 12.09,12.09 0 0 0 48.92,30.73 Z M 5,42.81 a 10.08,10.08 0 0 1 16.38,-7.87 l -0.2,0.22 -0.39,0.41 c -0.22,0.24 -0.44,0.49 -0.65,0.74 l -0.35,0.45 c -0.21,0.27 -0.4,0.55 -0.58,0.83 -0.18,0.28 -0.2,0.29 -0.29,0.44 -0.2,0.34 -0.4,0.7 -0.58,1.06 0,0.09 -0.1,0.18 -0.14,0.27 -0.22,0.46 -0.41,0.93 -0.59,1.41 0,0.12 -0.07,0.24 -0.11,0.36 -0.12,0.36 -0.23,0.73 -0.32,1.1 -0.05,0.17 -0.08,0.35 -0.12,0.52 -0.07,0.33 -0.13,0.66 -0.17,1 -0.04,0.34 -0.05,0.38 -0.07,0.57 -0.02,0.19 -0.05,0.28 -0.06,0.42 H 5 Z m 13.71,5.89 v -2.82 a 0.66,0.66 0 0 0 0,-0.14 13.28,13.28 0 0 1 21.75,-10 c 0.32,0.28 0.63,0.56 0.92,0.85 a 13.18,13.18 0 0 1 3.89,9.2 c 0,0 0,0.09 0,0.11 v 2.8 z M 59,44.7 H 47.24 c 0,-0.1 0,-0.19 0,-0.29 0,-0.1 -0.05,-0.41 -0.07,-0.62 C 47.15,43.58 47.05,43.3 47,43 46.95,42.7 46.92,42.59 46.87,42.39 46.82,42.19 46.74,41.88 46.67,41.62 L 46.49,41.03 C 46.4,40.78 46.31,40.54 46.21,40.29 46.11,40.04 46.06,39.91 45.98,39.73 45.9,39.55 45.75,39.24 45.63,39 c -0.12,-0.24 -0.17,-0.36 -0.27,-0.53 -0.1,-0.17 -0.29,-0.48 -0.44,-0.72 -0.15,-0.24 -0.19,-0.32 -0.3,-0.47 -0.11,-0.15 -0.37,-0.51 -0.57,-0.76 -0.2,-0.25 -0.18,-0.25 -0.28,-0.37 -0.3,-0.36 -0.62,-0.72 -1,-1.06 L 42.62,35 A 10.08,10.08 0 0 1 59,42.81 Z"
+       id="path66" fill="#D4FAFF"/>
+    <path
+       d="m 48.92,16.85 a 6.66,6.66 0 1 0 6.65,6.65 6.65,6.65 0 0 0 -6.65,-6.65 z m 0,11.31 a 4.66,4.66 0 1 1 4.65,-4.66 4.66,4.66 0 0 1 -4.65,4.66 z"
+       id="path68" fill="#D4FAFF"/>
+    <path
+       d="m 8.43,23.5 a 6.65,6.65 0 1 0 6.65,-6.65 6.66,6.66 0 0 0 -6.65,6.65 z m 11.3,0 a 4.65,4.65 0 1 1 -4.65,-4.65 4.66,4.66 0 0 1 4.65,4.65 z"
+       id="path70" fill="#D4FAFF"/>
+  </g>
+</svg>
+    );
+}
+
+function ToolsIcon() {
+    return (
+<svg fill="#000000" height="24px" width="24px"
+	 viewBox="0 0 489.347 489.347" >
+<g>
+	<path d="M412.642,345.939l-59.523-14.229l-66.352-66.352l51.12-51.055c11.874,4.167,24.216,6.203,36.499,6.202
+		c28.736-0.002,57.122-11.149,78.233-32.221c32.686-32.626,41.544-82.646,22.043-124.466l-9.042-19.391l-53.807,53.682
+		l-24.986-24.941l53.822-53.696L421.17,10.42C379.3-9.036,329.218-0.195,296.546,32.417
+		c-30.131,30.078-40.012,74.943-26.092,114.534l-20.111,20.086L102.13,18.837C91.061,7.731,76.32,1.605,60.621,1.587
+		c-0.023,0-0.044,0-0.067,0c-15.696,0-30.45,6.104-41.553,17.195C7.886,29.897,1.77,44.669,1.778,60.378
+		c0.008,15.697,6.129,30.456,17.233,41.553L167.18,250.094l-20.155,20.129c-39.652-13.917-84.597-4.061-114.733,26.02
+		C-0.393,328.869-9.252,378.888,10.25,420.708l9.042,19.391l53.806-53.681l24.986,24.94l-53.822,53.697l19.48,9.051
+		c14.814,6.883,30.652,10.224,46.388,10.224c28.738-0.001,57.124-11.148,78.235-32.221c30.132-30.078,40.013-74.943,26.093-114.534
+		l51.082-51.018l66.366,66.366l14.229,59.523l76.705,76.706l66.507-66.507L412.642,345.939z M301.691,144.194
+		c-14.181-30.419-7.73-66.807,16.05-90.545c18.28-18.246,44.036-26.278,68.827-22.6l-42.211,42.113l67.451,67.328l42.24-42.142
+		c3.697,24.738-4.343,50.456-22.622,68.702c-23.802,23.759-60.288,30.197-90.793,16.02l-9.505-4.417l-34.603,34.559l-24.968-24.965
+		l34.573-34.529L301.691,144.194z M31.778,60.362c-0.004-7.69,2.992-14.923,8.43-20.362c5.433-5.426,12.657-8.414,20.347-8.414
+		c7.711,0.009,14.918,3.002,20.345,8.446l194.398,194.38l-40.711,40.659L40.221,80.714C34.781,75.277,31.782,68.049,31.778,60.362z
+		 M167.171,430.877c-18.28,18.246-44.038,26.278-68.827,22.6l42.211-42.112l-67.451-67.329l-42.24,42.142
+		c-3.698-24.737,4.343-50.455,22.623-68.702c23.801-23.758,60.288-30.197,90.792-16.021l9.505,4.417l34.609-34.565l24.967,24.966
+		l-34.578,34.534l4.44,9.525C197.403,370.751,190.952,407.138,167.171,430.877z M373.342,397.227l-7.564-31.645l31.646,7.564
+		l49.498,49.499l-24.081,24.081L373.342,397.227z" fill="#D4FAFF"/>
+</g>
+</svg>
+    );
+}
+
+function ArrowIcon() {
+    return (
+        <svg className="arrow-icon" height="20" width="20" viewBox="0 0 330 330">
+            <path d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001
+                c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213
+                C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606
+                C255,161.018,253.42,157.202,250.606,154.389z"/>
+        </svg>        
+    );
+}
+
 function ProjectCard({project}) {
     return (
-      <div /*style={notableStyle.container}*/ className="project-card-container">
+      <div className="project-card-container">
         <div style={notableStyle.gifContainer}>
-            {/*<img 
-                src={`${process.env.PUBLIC_URL}/images/beehive.gif`}
-                alt="Project animation" 
-                style={notableStyle.gifImage}
-    />*/}
             <video 
-                src={`${process.env.PUBLIC_URL}/videos/${project.video}`} // Change the path to your video file
+                src={`${process.env.PUBLIC_URL}/videos/${project.video}`}
                 alt="Project animation" 
                 style={notableStyle.gifImage}
                 autoPlay 
                 loop 
-                muted // This is often necessary to autoplay in browsers
-                playsInline // Helps with autoplay on mobile devices
+                muted
+                playsInline
                 disablePictureInPicture
             ></video>
+            <div style={notableStyle.overlay}>
+                <div style={notableStyle.iconTextContainer}>
+                    <DurationIcon/>
+                    {project.duration}
+                </div>
+                <div style={notableStyle.iconTextContainer}>
+                    <PeopleIcon/>
+                    {project.group_size}
+                </div>
+                <div style={notableStyle.iconTextContainer}>
+                    <ToolsIcon/>
+                    {project.technologies}
+                </div>
+            </div>
         </div>
         <div style={cardContent}>
-            <span style={textStyles.titleText}>{project.name} ({project.year})</span>
+            <div style={titleContainer}>
+                <span style={textStyles.titleText}>{project.name} ({project.year})</span>
+            </div>
             <span style={textStyles.projectTypeText}>{project.type}</span>
-            <span style={textStyles.projectLanguageText}>Technologies: {project.technologies} / Languages: {project.languages}</span>
             <p style={textStyles.projectDescriptionText}>{project.short_desc}</p>
         </div>
+        <div className="read-more"><ArrowIcon/></div>
       </div>
     );
 }
@@ -44,7 +128,6 @@ const textStyles = {
         fontFamily: 'Kanit',
         fontSize: '16px',
         fontWeight: 300,
-        //letterSpacing: '1px',
         color: '#3c4059',
         marginTop: '0px',
         textTransform: 'uppercase',    
@@ -53,7 +136,6 @@ const textStyles = {
         fontFamily: 'Kanit',
         fontSize: '14px',
         fontWeight: 300,
-        //letterSpacing: '1px',
         color: '#3c4059',
         marginTop: '0px',
         textTransform: 'uppercase',   
@@ -69,21 +151,30 @@ const textStyles = {
 }
 
 const cardContent = {
-    height: '90%',
+    /*height: '90%',
     marginLeft: '15px',
     display: 'flex',
     flexDirection: 'column',
+    position: 'relative',*/
+    display: 'flex',
+    flexDirection: 'column',
     position: 'relative',
+    padding: '15px', // Added padding for overall internal spacing
 }
 
-const arrowContainer = {
-    position: 'absolute',
-    right: '40px',
-    bottom: '70px',
-    //transform: 'translateY(-50%)',
-    width: '20px',
-    height: '20px',
+const titleContainer = {
+    display: 'flex',
+    justifyContent: 'space-between', // This pushes the arrow icon to the far right
+    alignItems: 'center', // Vertically centers the title and arrow icon
 }
+
+const readMoreStyle = {
+    position: 'absolute', // Position relative to cardContent
+    bottom: '10px', // Adjust as necessary for spacing from the bottom
+    right: '15px', // Adjust as necessary for spacing from the right
+    color: '#3c4059', // Match the color scheme of your card
+    cursor: 'pointer', // Change cursor to pointer on hover to indicate it's clickable
+};
 
 const notableStyle = {
     container: {
@@ -101,15 +192,35 @@ const notableStyle = {
         border: '5px solid #fc9bd3',
     },
     gifContainer: {
+        position: 'relative',
         width: '100%',
-        //height: '150px',
+        height: 'auto',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
     },
     gifImage: {
-        width: '100%', // Adjust as needed
-        height: 'auto' // Adjust as needed
+        width: '100%',
+        height: '100%',
+        objectFit: 'cover',
     },
+    overlay: {
+        position: 'absolute',
+        bottom: 0,
+        width: '100%',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        color: '#d4faff',
+        textAlign: 'center',
+        padding: '10px 0',
+
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+    },
+    iconTextContainer: {
+        display: 'flex',
+        alignItems: 'center',
+        gap: '5px',
+    }
   }
 export default ProjectCard;
